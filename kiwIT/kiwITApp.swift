395d168ec0cfab6646440fe34482747531bc3136
phillyWork <hdsimonphilly@gmail.com>
2024-03-10 22:10:39 +0900

    Initial Commit

---
+++ kiwIT/kiwITApp.swift
@@ -0,0 +1,17 @@
+//
+//  kiwITApp.swift
+//  kiwIT
+//
+//  Created by Heedon on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct kiwITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
