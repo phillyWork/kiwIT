0b0df648b33649d56d534863d9de646696e6da3f
phillyWork <hdsimonphilly@gmail.com>
2024-04-29 23:44:58 +0900

    - Network Setup (Error case with error messages based on status code) - Basic NetworkManager request function (needs Combine framework application)

--- kiwIT/Manager/Network/Error+Network.swift
+++ kiwIT/Manager/Network/Error+Network.swift
@@ -9,9 +9,131 @@ import Foundation
 
 enum NetworkErrorCase {
     
+    case signUp
+    case signIn
+    case signOut
+    case refreshToken
+    case withdraw
+    case profileCheck
+    case profileEdit
+    case acquiredTrophyList
+    case mostRecentAcquiredTrophy
+    
+    //case summaryStat
+        
+    case lectureLevelListCheck
+    case lectureLevelListContentCheck
+    case startOfLecture
+    case completionOfLecture
+    case exerciseForLecture
+    case lectureCategoryListCheck
+    case lectureCategoryListContentCheck
+    case lectureNextStudyProgress
+    case completedLectureListCheck
+    case bookmarkedLectureCheck
+    case bookmarkLecture
+    
+    case quizListCheck
+    case startTakingQuiz
+    case submitQuizAnswers
+    case submitQuizAnswersNTimes
+    case mostRecentTakenQuiz
+    case takenQuizListCheck
+    case bookmarkedQuizCheck
+    case bookmarkQuiz
     
 }
 
-struct ErrorMessage {
+struct NetworkErrorMessage {
+    let status: Int
+    let errorCase: NetworkErrorCase
     
+    var message: String {
+        switch status {
+        case 202:
+            switch errorCase {
+            case .signIn:
+                return Setup.NetworkErrorMessage.signInError202
+            default:
+                return "Error Code 202"
+            }
+        case 204:
+            switch errorCase {
+            case .mostRecentAcquiredTrophy:
+                return Setup.NetworkErrorMessage.mostRecentTakenQuizError204
+            case .mostRecentTakenQuiz:
+                return Setup.NetworkErrorMessage.mostRecentTakenQuizError204
+            default:
+                return "Error Code 204"
+            }
+        case 400:
+            switch errorCase {
+            case .signUp:
+                return Setup.NetworkErrorMessage.signUpError
+            case .signIn:
+                return Setup.NetworkErrorMessage.signInError400
+            case .withdraw:
+                return Setup.NetworkErrorMessage.withdrawError400
+            case .profileCheck:
+                return Setup.NetworkErrorMessage.profileCheckError400
+            case .profileEdit:
+                return Setup.NetworkErrorMessage.profileEditError400
+            case .acquiredTrophyList:
+                return Setup.NetworkErrorMessage.acquiredTrophyListError400
+            case .mostRecentAcquiredTrophy:
+                return Setup.NetworkErrorMessage.mostRecentAcquiredTrophyError400
+            case .exerciseForLecture:
+                return Setup.NetworkErrorMessage.exerciseForLectureError400
+            case .lectureNextStudyProgress:
+                return Setup.NetworkErrorMessage.lectureNextStudyProgressError400
+            case .bookmarkLecture:
+                return Setup.NetworkErrorMessage.bookmarkLectureError400
+            case .quizListCheck:
+                return Setup.NetworkErrorMessage.quizListCheckError400
+            case .submitQuizAnswers:
+                return Setup.NetworkErrorMessage.submitQuizAnswersError400
+            case .submitQuizAnswersNTimes:
+                return Setup.NetworkErrorMessage.submitQuizAnswersNTimesError400
+            case .bookmarkQuiz:
+                return Setup.NetworkErrorMessage.bookmarkQuizError
+            default:
+                return "Error Code 400"
+            }
+        case 401:
+            //case별로 다른 메시지가 필요할 지, 혹은 내부적으로 어차피 access token refresh이므로 그냥 넘어가도 될 지 고민
+            return Setup.NetworkErrorMessage.invalidAccessToken
+        case 410:
+            switch errorCase {
+            case .lectureLevelListContentCheck:
+                return Setup.NetworkErrorMessage.lectureLevelListContentCheckError410
+            case .startOfLecture:
+                return Setup.NetworkErrorMessage.startOfLectureError410
+            case .completionOfLecture:
+                return Setup.NetworkErrorMessage.completionOfLectureError410
+            case .exerciseForLecture:
+                return Setup.NetworkErrorMessage.exerciseForLectureError410
+            case .lectureCategoryListContentCheck:
+                return Setup.NetworkErrorMessage.lectureCategoryListContentCheckError410
+            case .bookmarkLecture:
+                return Setup.NetworkErrorMessage.bookmarkLectureError410
+            case .startTakingQuiz:
+                return Setup.NetworkErrorMessage.startTakingQuizError410
+            case .submitQuizAnswers:
+                return Setup.NetworkErrorMessage.submitQuizAnswersError410
+            case .submitQuizAnswersNTimes:
+                return Setup.NetworkErrorMessage.submitQuizAnswersNTimesError410
+            default:
+                return "Error Code 410"
+            }
+        case 500:
+            switch errorCase {
+            case .profileEdit:
+                return Setup.NetworkErrorMessage.profileEditError500
+            default:
+                return "Error Code 500"
+            }
+        default:
+            return "Network Error: Invalid Case Error Messages"
+        }
+    }
 }
---
+++ kiwIT/Setup/Strings/Setup+NetworkErrorMessageStrings.swift
@@ -0,0 +1,141 @@
+//
+//  Setup+NetworkErrorMessages.swift
+//  kiwIT
+//
+//  Created by Heedon on 4/29/24.
+//
+
+import Foundation
+
+extension Setup {
+    enum NetworkErrorMessage {
+        
+        static let invalidAccessToken = "요청 권한이 존재하지 않음. 토큰 리프레시 필요."
+        
+        
+        static let signUpError = "중복된 닉네임이 존재합니다. 다른 닉네임을 입력해주세요."
+        static let signInError202 = "회원가입이 필요합니다. 다음을 입력해주세요."
+        static let signInError400 = "소셜 로그인에 오류가 발생했습니다. 다시 시도해주세요."
+       
+        //내부적 처리?
+        static let signOutError = "로그아웃 권한이 없습니다.(토큰 리프레시 필요)"
+        
+        static let refreshTokenError = "사용 권한이 만료되었습니다. 다시 로그인해주세요."
+        
+        //내부적 처리?
+        static let profileCheckError400 = "유저 정보가 존재하지 않습니다."
+        static let profileCheckError401 = "유저 정보 확인 권한이 없습니다.(토큰 리프레시 필요)"
+
+        static let profileEditError400 = "중복된 닉네임이 존재합니다. 다른 닉네임을 입력해주세요."
+        
+        //내부적 처리?
+        static let profileEditError401 = "유저 정보 수정 권한이 없습니다.(토큰 리프레시 필요)"
+
+        //미정된 API doc
+        static let profileEditError500 = "미확인 사유(분리 예정 필요)"
+        
+        
+        static let withdrawError400 = "잘못된 회원탈퇴 요청입니다. 다시 시도해주세요."
+        
+        //내부적 처리?
+        static let withdrawError401 = "회원 탈퇴 요청 권한이 없습니다. (토큰 리프레시 필요)"
+        
+        
+        static let acquiredTrophyListError400 = "잘못된 요청입니다. 다시 시도해주세요."
+        
+        //내부적 처리?
+        static let acquiredTrophyListError401 = "트로피 획득 내역 확인 권한이 없습니다. (토큰 리프레시 필요)"
+        
+        
+        static let mostRecentAcquiredTrophyError204 = "획득한 트로피가 존재하지 않습니다."
+        
+        static let mostRecentAcquiredTrophyError400 = "잘못된 요청입니다. 다시 시도해주세요."
+       
+        //내부적 처리?
+        static let mostRecentAcquiredTrophyError401 = "가장 최근 획득한 트로피 확인 권한이 없습니다. (토큰 리프레시 필요)"
+        
+        
+        //case summaryStat
+       
+        
+        
+        //내부적 처리?
+        static let lectureLevelListCheckError = "레벨 리스트 조회 권한이 없습니다. (토큰 리프레시 필요)"
+        static let lectureLevelListContentCheckError401 = "레벨 리스트의 컨텐츠 조회 권한이 없습니다. (토큰 리프레시 필요)"
+        
+        static let lectureLevelListContentCheckError410 = "해당 레벨의 컨텐츠가 존재하지 않습니다."
+        
+        //내부적 처리?
+        static let startOfLectureError401 = "학습 시작 권한이 없습니다. (토큰 리프레시 필요)"
+
+        static let startOfLectureError410 = "해당 학습 컨텐츠가 존재하지 않습니다."
+        
+        
+        //내부적 처리?
+        static let completionOfLectureError401 = "학습 완료 권한이 없습니다. (토큰 리프레시 필요)"
+        
+        static let completionOfLectureError410 = "해당 학습 컨텐츠가 존재하지 않습니다."
+
+        
+        static let exerciseForLectureError400 = "정답을 입력해야 합니다."
+
+        //내부적 처리?
+        static let exerciseForLectureError401 = "예제 문제 풀이 권한이 없습니다. (토큰 리프레시 필요)"
+        
+        static let exerciseForLectureError410 = "해당 학습 컨텐츠가 존재하지 않습니다."
+        
+        
+        //내부적 처리?
+        static let lectureCategoryListCheckError = "과목 리스트 조회 권한이 없습니다. (토큰 리프레시 필요)"
+        static let lectureCategoryListContentCheckError401 = "과목 리스트의 컨텐츠 조회 권한이 없습니다. (토큰 리프레시 필요)"
+        
+        static let lectureCategoryListContentCheckError410 = "해당 과목의 컨텐츠가 존재하지 않습니다."
+        
+        static let lectureNextStudyProgressError400 = "이전에 학습한 내용이 존재하지 않습니다."
+      
+        //내부적 처리?
+        static let lectureNextStudyProgressError401 = "다음 학습 진도 확인 권한이 없습니다. (토큰 리프레시 필요)"
+        static let completedLectureListCheckError = "학습 완료 리스트 확인 권한이 없습니다. (토큰 리프레시 필요)"
+        static let bookmarkedLectureCheckError = "보관한 학습 컨텐츠 확인 권한이 없습니다. (토큰 리프레시 필요)"
+
+        
+        static let bookmarkLectureError400 = "컨텐츠 보관함 처리에 실패했습니다. 다시 시도해주세요."
+        static let bookmarkLectureError410 = "보관함 처리할 해당 학습 컨텐츠가 존재하지 않습니다."
+        
+     
+        static let quizListCheckError400 = "잘못된 퀴즈 리스트 요청입니다. 다시 시도해주세요."
+        
+        //내부적 처리?
+        static let quizListCheckError401 = "퀴즈 리스트 요청 권한이 없습니다. (토큰 리프레시 필요)"
+        static let startTakingQuizError401 = "퀴즈 풀이 권한이 없습니다. (토큰 리프레시 필요)"
+        
+        static let startTakingQuizError410 = "해당 퀴즈는 조회할 수 업습니다."
+        
+        static let submitQuizAnswersError400 = "모든 문제의 답안을 작성해야 제출할 수 있습니다."
+        
+        //내부적 처리?
+        static let submitQuizAnswersError401 = "문제 답안 제출 권한이 없습니다. (토큰 리프레시 필요)"
+    
+        static let submitQuizAnswersError410 = "해당 퀴즈는 문제 풀이가 불가능합니다."
+        
+
+        static let submitQuizAnswersNTimesError400 = "모든 문제의 답안을 작성해야 제출할 수 있습니다."
+        
+        //내부적 처리?
+        static let submitQuizAnswersNTimesError401 = "문제 답안 제출 권한이 없습니다. (토큰 리프레시 필요)"
+        
+        static let submitQuizAnswersNTimesError410 = "해당 퀴즈는 문제 풀이가 불가능합니다."
+        
+     
+        static let mostRecentTakenQuizError204 = "최근 답안을 제출한 퀴즈가 존재하지 않습니다."
+        
+        //내부적 처리?
+        static let mostRecentTakenQuizError401 = "가장 최근에 답안을 제출한 퀴즈 확인 권한이 없습니다. (토큰 리프레시 필요)"
+        static let takenQuizListCheckError = "답안을 제출한 퀴즈 리스트 확인 권한이 없습니다. (토큰 리프레시 필요)"
+        static let bookmarkedQuizCheckError = "보관한 퀴즈 확인 권한이 없습니다. (토큰 리프레시 필요)"
+        
+       
+        case bookmarkQuizError = "퀴즈 보관함 처리에 실패했습니다. 다시 시도해주세요."
+        
+    }
+}
