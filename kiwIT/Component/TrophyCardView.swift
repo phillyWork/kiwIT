5f6d58c8f9fcbf8b0375241bd5ef479102e7c350
phillyWork <hdsimonphilly@gmail.com>
2024-07-24 01:45:12 +0900

    - remove summarystat (API deletion) - update trophy model with lecture & quiz API response model - basic UI setup for showing new acquired trophy with transparent background cardview

---
+++ kiwIT/Component/TrophyCardView.swift
@@ -0,0 +1,18 @@
+//
+//  TrophyCardView.swift
+//  kiwIT
+//
+//  Created by Heedon on 7/24/24.
+//
+
+import SwiftUI
+
+struct TrophyCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TrophyCardView()
+}
