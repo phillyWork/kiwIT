b199457558e217f263f8a8b6414d26de629f2fb9
phillyWork <hdsimonphilly@gmail.com>
2024-08-02 01:51:41 +0900

    - UI update for trophy content

--- kiwIT/Component/ProfileContent/TrophyContent/TrophyContent.swift
+++ kiwIT/Component/ProfileContent/TrophyContent/TrophyContent.swift
@@ -9,31 +9,19 @@ import SwiftUI
 
 struct TrophyContent: View {
     
-//    var acquiredTrophyData: AcquiredTrophy
-    
-//    var trophyData: TrophyEntity
-    
-    var tempTrophyTitle: String
-    var tempTrophyExplanation: String
-    var tempImageUrlString: String
+    var trophy: TrophyEntity
     var achievedDate: String?
-
+    
     var body: some View {
         ZStack(alignment: .center) {
             Rectangle()
-                .fill(achievedDate != nil ? Color.shadowColor : .clear)
+                .fill(Color.shadowColor)
                 .frame(width: Setup.Frame.trophyContentWidth, height: Setup.Frame.trophyContentHeight)
                 .offset(CGSize(width: Setup.Frame.contentListShadowWidthOffset, height: Setup.Frame.contentListShadowHeightOffset))
             HStack {
-                AsyncImage(url: URL(string: tempImageUrlString)) { image in
-                    //ViewModel에서 변환 처리?
-                    
-                    //                let renderer = ImageRenderer(content: image)
-                    //                if let uiImage = renderer.uiImage {
-                    //                    self.imageForExpandable = uiImage
-                    //                }
-                    
+                AsyncImage(url: URL(string: trophy.imageUrl)) { image in
                     image.resizable()
+                        .scaledToFit()
                 } placeholder: {
                     ProgressView()
                 }
@@ -41,8 +29,8 @@ struct TrophyContent: View {
                 .grayscale(achievedDate != nil ? 0.0 : 1.0)
                 
                 VStack {
-                    Text(tempTrophyTitle)
-                    Text(tempTrophyExplanation)
+                    Text(trophy.title)
+                    Text("\(trophy.type.description): \(trophy.threshold)")
                         .font(.custom(Setup.FontName.notoSansMedium, size: 12))
                     if let date = achievedDate {
                         Text("획득 날짜: \(date)")
@@ -62,7 +50,7 @@ struct TrophyContent: View {
 
 #Preview {
     VStack {
-        TrophyContent(tempTrophyTitle: "지금까지 수고한 당신, 최고에요!", tempTrophyExplanation: "누적 학습시간 100시간 돌파", tempImageUrlString: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHZCbqRAGGwuZYEDajBgZx1zUgTdqBwfwVZw&s", achievedDate: "2024-02-11")
-        TrophyContent(tempTrophyTitle: "이정도로 해낼 줄이야!", tempTrophyExplanation: "누적 학습시간 300시간 돌파", tempImageUrlString: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHZCbqRAGGwuZYEDajBgZx1zUgTdqBwfwVZw&s")
+        TrophyContent(trophy: TrophyEntity(id: 12, title: "지금까지 수고한 당신, 최고에요!", imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHZCbqRAGGwuZYEDajBgZx1zUgTdqBwfwVZw&s", type: .totalContentStudied, threshold: 100), achievedDate: "2024-02-11")
+        TrophyContent(trophy: TrophyEntity(id: 14, title: "이정도로 해낼 줄이야!", imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHZCbqRAGGwuZYEDajBgZx1zUgTdqBwfwVZw&s", type: .totalContentStudied, threshold: 300))
     }
 }
